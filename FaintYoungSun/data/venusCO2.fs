7d7c470f3d43c56178192d244d3e4ae9f7204d2c
Aaron Geller <a-geller@northwestern.edu>
2018-03-29 11:33:16 -0500

    added Venus

--- FaintYoungSun/data/venusCO2.fs
+++ FaintYoungSun/data/venusCO2.fs
@@ -10,10 +10,13 @@ out vec4 fragColor;
 void main()
 {	
 
-	float tmin = 1000.;
-	float tdur = 1000.;
-	float tdiff = abs(simUseTime - tmin);
-	float r = clamp(tdiff/tdur, 0.2, 0.7);
+	//to start with some, decrease and then increase again
+	//float tmin = 1000.;
+	//float tdur = 100.;
+	//float tdiff = abs(simUseTime - tmin);
+	//float r = clamp(tdiff/tdur, 0.2, 0.7);
+	
+	float r = clamp(simUseTime/4500., 0.2, 0.7);
 	fragColor = vec4(r, 0., 0., r);
 	fragColor.a *= uv_fade * uv_alpha;
 
--- FaintYoungSun/data/venusWater.fs
+++ FaintYoungSun/data/venusWater.fs
@@ -1,18 +1,25 @@
 uniform float uv_fade;
 uniform float uv_alpha;
 uniform sampler2D VenusTexture;
+uniform float simWaterFac;
+uniform float simUseTime;
 
-flat in float DistanceFade;
 in vec2 TexCoord;
-
-uniform vec3 glowColor;
 
 out vec4 FragColor;
 
 void main()
 {	
 	vec4 mapColor = texture(VenusTexture,TexCoord);
-    mapColor.a*=uv_alpha*uv_fade;
+	float fac = simWaterFac * clamp((1. - simUseTime/4500.), 0, 1);
+	float fade = 1. - fac*(1. - mapColor.a);
+	float rfac = clamp(0.6*fade*fade - 1., -1, 0);
+	float bfac = clamp((1. - 2.*fade*fade), 0, 1.);
+	float gfac = clamp((0.7 - 4. * (fade - 0.6) * (fade - 0.6)) - bfac, -1, 0.0);
+	mapColor.r += fac*rfac;
+	mapColor.g += fac*gfac;
+	mapColor.b += fac*bfac;
+    mapColor.a = uv_alpha*uv_fade;
 	FragColor = mapColor;
 
 }
